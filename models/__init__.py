f9d97fc05e7e22442813468311144be77f311688
Ismail <khalfaoui.ismail@hotmail.com>
2023-10-16 12:00:40 +0200

    remove unused imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,2 @@
-from .metaformer_baselines import *
-from .test_downsample import *
-from .parcnet_v3 import *
-from .parcnet_v3_bgu import *
 from .parcnetv2 import *
-from .poolformer_bgu import *
+
